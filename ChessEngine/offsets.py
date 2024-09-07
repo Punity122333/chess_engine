25244d73d4024907a344b4c0e4cabba2bfc57c2d
Parthiv Pal <66818808+Punity122333@users.noreply.github.com>
2024-09-07 22:47:52 +0530

    Delete ChessEngine directory
    new one upload

--- ChessEngine/offsets.py
+++
@@ -1,5 +0,0 @@
-knight_offets = [(1, 2), (1, -2), (-1, 2), (-1, -2), (2, 1), (2, -1), (-2, 1), (-2, -1)]
-rook_offsets = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-bishop_offsets = [(1, 1), (1, -1), (-1, 1), (-1, -1)]
-queen_offsets = rook_offsets + bishop_offsets
-king_offsets = queen_offsets.copy()
